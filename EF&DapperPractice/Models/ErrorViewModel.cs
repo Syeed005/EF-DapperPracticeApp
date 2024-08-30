f418ece4b77cdbc02c2ee5fe53cc0f7e52304952
Rahman Syeedur <rahman.syeedur@valmetpartners.com>
2024-08-30 17:00:17 -0500

    Add project files.

---
+++ EF&DapperPractice/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace EF_DapperPractice.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
